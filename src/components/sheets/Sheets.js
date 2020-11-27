fd25e07874a659802912936fded504c5f00d47f1
Andrey Golik <1858191+andreygolik@users.noreply.github.com>
2020-11-27 18:20:55 -0500

    dom

--- src/components/sheets/Sheets.js
+++ src/components/sheets/Sheets.js
@@ -2,7 +2,7 @@ import { $ } from '@core/dom'
 
 export class Sheets {
   constructor(selector, options) {
-    this.$el = document.querySelector(selector)
+    this.$el = $(selector)
     this.components = options.components || []
   }
 
@@ -12,7 +12,7 @@ export class Sheets {
     this.components = this.components.map((Component) => {
       const $el = $.create('div', Component.className)
       const component = new Component($el)
-      $el.innerHTML = component.toHTML()
+      $el.html(component.toHTML())
       $root.append($el)
       return component
     })
